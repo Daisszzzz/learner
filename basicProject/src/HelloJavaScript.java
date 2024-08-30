423b3dd1c68b68e5724abdcc49afb3ab119db0a6
ShengzongDai <u7811526@anu.edu.au>
2024-08-30 20:16:25 +1000

    update HelloJavaScript

---
+++ basicProject/src/HelloJavaScript.java
@@ -0,0 +1,10 @@
+/**
+ * @ClassName HelloJavaScript
+ * @Author lenovo
+ * @Date 2024/8/30 20:15
+ **/
+public class HelloJavaScript {
+    public static void main(String[] args) {
+        System.out.println("Hello, JavaScript");
+    }
+}
