85a29e18341091d890a0198cbd289030769714e6
ShengzongDai <u7811526@anu.edu.au>
2024-08-30 19:54:01 +1000

    update HelloJava

---
+++ basicProject/src/HelloJava.java
@@ -0,0 +1,10 @@
+/**
+ * @ClassName HelloJava
+ * @Author lenovo
+ * @Date 2024/8/30 19:53
+ **/
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("HelloJava");
+    }
+}
